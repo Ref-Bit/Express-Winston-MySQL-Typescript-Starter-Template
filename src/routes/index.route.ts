0649a54744035c27cf3b74b536817b0a5341ea61
RefBit <refaatbitar@gmail.com>
2024-10-07 11:09:19 +0300

    Initialized

---
+++ src/routes/index.route.ts
@@ -0,0 +1,12 @@
+import { Request, Response, Router } from 'express';
+
+const router = Router();
+
+router.get('/health-check', (req: Request, res: Response) => {
+  res.json({
+    success: true,
+    message: 'Successfully connected to API.',
+  });
+});
+
+export default router;
